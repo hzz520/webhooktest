0e4d07bb363c203f7743977ffb7198622872c382
huangzhongzhen <1013452861@qq.com>
2019-09-23 19:13:32 +0800

    hzz

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ handler.on('error', function (err) {
 })
 
 handler.on('push', function (event) {
-  console.log(event.path)
+  console.log(JSON.stringify(event))
   console.log('Received a push event for %s to %s',
     event.payload.repository.name,
     event.payload.ref)
