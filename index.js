64b2be44765f6269f21ac0ace6beaaac414e4c4c
huangzhongzhen <1013452861@qq.com>
2019-09-24 20:45:58 +0800

    hzz

--- index.js
+++ index.js
@@ -17,23 +17,24 @@ http.createServer(function (req, res) {
         res.statusCode = 404;
         res.end('no such location')
       }))
-
-      handler.on('error', function (err) {
-        console.error('Error:', err.message)
-      })
-      
-      handler.on('push', function (event) {
-        console.log('Received a push event for %s to %s',
-          event.payload.repository.name,
-          event.payload.ref)
-        rumCommand('sh', ['./deployed.sh', event.payload.repository.name], function (txt) {
-          console.log(txt)
-        })
-      })
+    } else {
+        res.statusCode = 404;
+        res.end('no such location')
     }
-    res.statusCode = 404;
-    res.end('no such location')
 }).listen(8900)
+
+handler.on('error', function (err) {
+    console.error('Error:', err.message)
+  })
+  
+handler.on('push', function (event) {
+    console.log('Received a push event for %s to %s',
+      event.payload.repository.name,
+      event.payload.ref)
+    rumCommand('sh', ['./deployed.sh', event.payload.repository.name], function (txt) {
+      console.log(txt)
+    })
+})
 
 function rumCommand(cmd, args, callback) {
   var child = spawn(cmd, args)
