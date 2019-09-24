2bf611b9fea0105e4fc2859aac73605bd75d154e
huangzhongzhen <1013452861@qq.com>
2019-09-24 21:46:49 +0800

    hzz

--- index.js
+++ index.js
@@ -11,38 +11,38 @@ function getHandler(path) {
 var handler = http.createServer(function (req, res) {
     console.log(req.url)
     if (/^\/pushCode/.test(req.url)) {
-      handler1 = getHandler(req.url)
+      var handler1 = getHandler(req.url)
 
       handler1(req, res, function (err) {
         res.statusCode = 404;
         res.end('no such location')
       })
+      handler1.on('error', function (err) {
+        console.error('Error:', err.message)
+      })
+      
+        handler1.on('push', function (event) {
+            console.log('Received a push event for %s to %s',
+            event.payload.repository.name,
+            event.payload.ref)
+            rumCommand('sh', ['./deployed.sh', event.payload.repository.name], function (txt) {
+            console.log(txt)
+            })
+        })
+        
+        function rumCommand(cmd, args, callback) {
+        var child = spawn(cmd, args)
+        var response = ''
+        child.stdout.on('data', function (buffer) {
+            response += buffer.toString()
+        })
+        child.stdout.on('end', function () {
+            callback(response)
+        })
+        }
     } else {
         res.statusCode = 404;
         res.end('no such location')
     }
 }).listen(8900)
 
-handler1.on('error', function (err) {
-    console.error('Error:', err.message)
-  })
-  
-handler1.on('push', function (event) {
-    console.log('Received a push event for %s to %s',
-      event.payload.repository.name,
-      event.payload.ref)
-    rumCommand('sh', ['./deployed.sh', event.payload.repository.name], function (txt) {
-      console.log(txt)
-    })
-})
-
-function rumCommand(cmd, args, callback) {
-  var child = spawn(cmd, args)
-  var response = ''
-  child.stdout.on('data', function (buffer) {
-    response += buffer.toString()
-  })
-  child.stdout.on('end', function () {
-    callback(response)
-  })
-}
