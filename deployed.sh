f0cc0fe77c1cc4b04471229eb28bcd9297ad04fa
huangzhongzhen <1013452861@qq.com>
2019-09-30 17:34:17 +0800

    hhzz

--- deployed.sh
+++ deployed.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
-echo '$1'
+echo $1
 cd /test/git/$1
 
-if [! ls './.git')];then
-    echo 'no'
+if [ ! -d /test/git/$1 ];then
+    git clone git@github.com:hzz520/$1.git
     chmod -R 777 './'
-    git clone git@github.com:hzz520/webhooktest.git
 else
-    echo 'yes'
     git pull origin master
 fi
 
 cnpm i
-if []
 pm2 restart $1
 pm2 log $1
